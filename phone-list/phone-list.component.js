7453b2d47e95de0a14aa405105890e82096878ac
David Basil <info@basildavid.com>
2017-08-16 18:52:58 +0400

    add phones.json

--- phone-list/phone-list.component.js
+++ phone-list/phone-list.component.js
@@ -2,24 +2,14 @@ angular.module('phoneList').component('phoneList', {
 	// template
 	templateUrl: 'phone-list/phone-list.template.html',
 	// controller
-	controller: function PhoneListController() {
-		this.phones = [
-			{
-				name: 'Nexus S',
-				snippet: 'Fast just got faster with Nexus S.',
-				age: 1
-			},
-			{
-				name: 'Nexus S',
-				snippet: 'The Next, Next Generation tablet.',
-				age: 2
-			},
-			{
-				name: 'MOTOROLA XOOM',
-				snippet: 'The next, next generation tablet.',
-				age: 3
-			}
-		]
-		this.orderProp = 'age'
-	}
+	controller: ['$http',
+		function PhoneListController($http){
+			var self = this
+			self.orderProp = 'age'
+			// http
+			$http.get('phones/phones.json').then(function(response){
+				self.phones = response.data
+			})
+		}
+	]
 })
