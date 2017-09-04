bb548f1babe6b2ebcd2a827f339f1c1b25a37390
David Basil <info@basildavid.com>
2017-09-04 21:30:18 +0400

    add greeting controller

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,5 @@
 angular.module('myApp.controllers', [])
-	.controller('FinanceController', function($scope){
-		$scope.salary = 0
-		$scope.percentage = 0
-		$scope.result = function(){
-			return $scope.salary * $scope.percentage * 0.01
-		}
+	.controller('GreetingController', function($scope){
+		$scope.now = new Date()
+		$scope.greeting = 'Hello' 
 	})
