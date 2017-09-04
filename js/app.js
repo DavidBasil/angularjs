6dfcee43c5b8648f32bf2d39b2758fb0d68253b3
David Basil <info@basildavid.com>
2017-09-05 01:00:53 +0400

    set up routes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 'use strict'
 
 angular.module('myApp', [
-	'myApp.controllers'
+	'myApp.controllers',
+	'ngRoute'
 ])
 
-angular.module('myApp').run(function($rootScope){
-	$rootScope.title = 'Famous books'
-	$rootScope.name = 'Root Scope'
+angular.module('myApp').config(function($routeProvider, $locationProvider){
+	$routeProvider.when('/view1', {
+		controller: 'Controller1',
+		templateUrl: 'partials/view1.html'
+	}).when('/view2', {
+		controller: 'Controller2',
+		templateUrl: 'partials/view2.html'
+	})
+	$locationProvider.html5Mode(true)
 })
--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,8 @@
-angular.module('myApp.controllers', [])
-	.controller('SiteController', function($scope){
-		$scope.publisher = 'SitePoint'
-		$scope.type = 'Web Development'
-		$scope.name = 'Scope for SiteController'
-	})
+'use strict'
 
-angular.module('myApp.controllers')
-	.controller('BookController', function($scope){
-		$scope.books = ['Jump start HTML5', 'Jump start CSS', 'Jump Start Responsive Web Design']
-		$scope.name = 'Scope for BookController'
-})
+angular.module('myApp.controllers', [])
+	.controller('Controller1', function($scope){
+		$scope.message = 'Hello, world'
+	}).controller('Controller2', function($scope){
+		$scope.now = new Date()
+	})
