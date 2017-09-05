95f4025680d1851a0bd36d0c030a4b9a10f4e5c2
David Basil <info@basildavid.com>
2017-09-05 14:25:39 +0400

    add ng-include

--- js/app.js
+++ js/app.js
@@ -8,10 +8,17 @@ angular.module('myApp', [
 angular.module('myApp').config(function($routeProvider, $locationProvider){
 	$routeProvider.when('/view1', {
 		controller: 'Controller1',
-		templateUrl: 'partials/view1.html'
-	}).when('/view2', {
+		templateUrl: '/partials/view1.html'
+	}).when('/view2/:firstname/:lastname', {
 		controller: 'Controller2',
-		templateUrl: 'partials/view2.html'
+		templateUrl: '/partials/view2.html',
+		resolve: {
+			names: function(){
+				return ['Misko', 'Vojta', 'Brad']
+			}
+		}
+	}).otherwise({
+		redirectTo: '/view1'
 	})
 	$locationProvider.html5Mode(true)
 })
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,12 @@
 'use strict'
 
 angular.module('myApp.controllers', [])
-	.controller('Controller1', function($scope){
-		$scope.message = 'Hello, world'
-	}).controller('Controller2', function($scope){
-		$scope.now = new Date()
+	.controller('Controller1', function($scope, $location){
+		$scope.loadView2 = function(){
+			$location.path('/view2/' + $scope.firstname + '/' + $scope.lastname)
+		}
+	}).controller('Controller2', function($scope, $routeParams, names){
+		$scope.firstname = $routeParams.firstname
+		$scope.lastname = $routeParams.lastname
+		$scope.names = names
 	})
